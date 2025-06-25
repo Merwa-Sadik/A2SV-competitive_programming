e49fe719f6d2264df79e6ae3bb4b1f39ea171e04
Merwa Sadik <bomar7141@gmail.com>
2025-06-25 20:39:51 +0300

    Created 'Letter Combinations of a Phone Number' by A2SV Hub

---
+++ 25-Jun-2025/Letter Combinations of a Phone Number 370447.py
@@ -0,0 +1,30 @@
+# Problem: Letter Combinations of a Phone Number - https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        digits_map = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+        result = []
+        def backtrack(index, path):
+            if index == len(digits):
+               result.append(path)
+               return
+
+            letters = digits_map[digits[index]]
+
+            for letter in letters:
+                backtrack(index + 1, path + letter)
+        backtrack(0, "")
+        return result
+
+        
